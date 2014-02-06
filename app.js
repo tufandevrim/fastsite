23eebc3815471a823156db5e6a2c45c73ce9d669
Devrim Tufan <tufandevrim@gmail.com>
2014-02-06 04:00:47 -0800

    deleting some folders

--- app.js
+++ app.js
@@ -4,7 +4,6 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
 var http = require('http');
 var path = require('path');
 
@@ -19,23 +18,12 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-app.use(function (req, res, next) {
-	res.set('X-Powered-By', 'Flight Tracker');
-	next();
-});
-app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
-
-app.get('/flight/:number', routes.flight);
-app.put('/flight/:number/arrived', routes.arrived);
-app.get('/list', routes.list);
-app.get('/test1', routes.test1);
-
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
