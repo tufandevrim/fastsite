b6d760385f290669f139998847c9225a235ab59a
Devrim Tufan <tufandevrim@gmail.com>
2022-02-07 15:52:30 -0800

    Enable beacon api via script

--- public/js/test_tracking.js
+++ public/js/test_tracking.js
@@ -1,4 +1,4 @@
-/*! For license information please see track_base_eb16952bb418d726f749.js.LICENSE.txt */
+/*! For license information please see track_base_d7f871062bfbc6a38e34.js.LICENSE.txt */
 !function(e){var t={}
 function r(n){if(t[n])return t[n].exports
 var o=t[n]={i:n,l:!1,exports:{}}
@@ -287,11 +287,10 @@ var r="- ".concat(null===(e=arguments[1])||void 0===e?void 0:e.item)||!1
 t[0]="%c ".concat(arguments[0]," ").concat(r)
 t.splice(1,0,"background-color:#B8EC51;color:#006344;font-weight:bold;font-family:system-ui;font-size:20px;")
 console.log.apply(console,t)}}function qe(e,t,r,n){Ee("client_view",e,t=t||{},r,n=n||!1)}var Be={CONTENT_TYPES:{CLASSIFIED:"classified",COMMENT:"comment",EVENT:"event",FSF:"fsf",LISTING:"listing",OFFER:"offer",PAGE:"page",POST:"post",REPLY:"reply"},SCOPE_TYPES:{CLASSIFIED:"classified",FEED:"feed",OFFERS:"offers",SEARCH:"search_result",TOPIC:"topic_page",LOCAL:"local",EVENTS:"events",MEDIA_DETAIL:"media_detail"}}
-t.default=n
+if("undefined"!=typeof window){window.unstable__preload_features=window.unstable__preload_features||{}
+window.unstable__preload_features.enable_beacon_api=!0}t.default=n
 if("undefined"!=typeof window){window.nd=window.nd||{}
-window.nd.flaskTrack=n
-window.unstable__preload_features=window.unstable__preload_features||{}
-window.unstable__preload_features.enable_beacon_api=!0}},"./node_modules/axios/index.js":function(e,t,r){e.exports=r("./node_modules/axios/lib/axios.js")},"./node_modules/axios/lib/adapters/xhr.js":function(e,t,r){"use strict"
+window.nd.flaskTrack=n}},"./node_modules/axios/index.js":function(e,t,r){e.exports=r("./node_modules/axios/lib/axios.js")},"./node_modules/axios/lib/adapters/xhr.js":function(e,t,r){"use strict"
 var n=r("./node_modules/axios/lib/utils.js")
 var o=r("./node_modules/axios/lib/core/settle.js")
 var i=r("./node_modules/axios/lib/helpers/buildURL.js")
@@ -817,4 +816,4 @@ return t||o(a)}},"./node_modules/webpack/buildin/global.js":function(e,t){var r
 r=function(){return this}()
 try{r=r||new Function("return this")()}catch(e){"object"==typeof window&&(r=window)}e.exports=r}})
 
-//# sourceMappingURL=track_base_eb16952bb418d726f749.js.map
+//# sourceMappingURL=track_base_d7f871062bfbc6a38e34.js.map
